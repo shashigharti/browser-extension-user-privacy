5c88fc20f7d4d49c24e49cd7ee1fb1ae222b1fca
Shashi Gharti <shashi.gharti@gmail.com>
2021-05-26 17:30:11 +0545

    included webext library from firefox as  submodule

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
-import { ToxicityClassifier } from "@tensorflow-models/toxicity";
 import React, { useState } from "react";
 import './App.css';
-import '@tensorflow/tfjs-backend-webgl';
-import '@tensorflow/tfjs-backend-cpu';
 import Loader from './utils/loader';
 import {
     CookieInstrument,
+    DnsInstrument,
     HttpInstrument,
     JavascriptInstrument,
     NavigationInstrument
-} from "@openwpm/webext-instrumentation";
+} from "openwpm-webext-instrumentation";
 import * as dataReceiver from "./utils/dataReceiver";
-
-
-var toxicity = require('@tensorflow-models/toxicity');
-
 
 export const App = () => {
     const [value, setValue] = React.useState('you suck');
@@ -22,33 +16,33 @@ export const App = () => {
     const [threshold, setThreshold] = useState(0.9);
     const [loading, setLoading] = useState(false);
 
-    async function getToxicity(){
-        setLoading(true)
-        toxicity.load(threshold).then((model : ToxicityClassifier) => {
-            const sentences = ['you suck'];
+    // async function getToxicity(){
+    //     setLoading(true)
+    //     toxicity.load(threshold).then((model : ToxicityClassifier) => {
+    //         const sentences = ['you suck'];
 
-            model.classify(sentences).then(predictions => {
-                Object.entries(predictions).forEach(([key, value]) => {
-                    if (value.label == "insult"){
-                        setLoading(false)
-                        setResponseFromContent(value.results[0].probabilities[1]);
-                    }                    
-                  });                
-            });
-        });
-    }
+    //         model.classify(sentences).then(predictions => {
+    //             Object.entries(predictions).forEach(([key, value]) => {
+    //                 if (value.label == "insult"){
+    //                     setLoading(false)
+    //                     setResponseFromContent(value.results[0].probabilities[1]);
+    //                 }                    
+    //               });                
+    //         });
+    //     });
+    // }
     const sendTestMessage = () => {
         // getToxicity();
-        const config = getOpenwpmConfig();
-        console.log("config" + config)
-        dataReceiver.activeTabDwellTimeMonitor.run();
+        // const config = getOpenwpmConfig();
+        // console.log("config" + config)
+        // dataReceiver.activeTabDwellTimeMonitor.run();
 
     };
     
     return (
         <div className="App">
             <h1>                
-                Toxicity Predictor
+                OpenWPM
             </h1>
             <table>
                 <tbody>
